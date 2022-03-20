b3eee2279c7ddb82bbc93bb88bf7495ee5f57f22
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-20 07:32:59 +0700

    #7 - Add deactivate & findByActiveTrue query method in CategoryRepository

--- src/main/java/com/hendisantika/repository/CategoryRepository.java
+++ src/main/java/com/hendisantika/repository/CategoryRepository.java
@@ -1,7 +1,14 @@
 package com.hendisantika.repository;
 
 import com.hendisantika.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,5 +20,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Time: 07.31
  * To change this template use File | Settings | File Templates.
  */
-public interface CategoryRepository extends JpaRepository<Category, Long>
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Modifying
+    @Query(value = "update Category category set category.createdAt = CURRENT_DATE where category.id = :id")
+    void deactivate(@Param("id") Long id);
+
+    List<Category> findByActiveTrue();
+
+    Page<Category> findByActiveTrue(Pageable pageable);
 }
