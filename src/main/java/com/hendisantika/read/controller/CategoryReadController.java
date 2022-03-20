2a355b88da045f149e3f143bba7a522a871a00e1
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-20 07:39:21 +0700

    #18 - Add getAll in CategoryReadController

--- src/main/java/com/hendisantika/read/controller/CategoryReadController.java
+++ src/main/java/com/hendisantika/read/controller/CategoryReadController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.read.controller;
 
+import com.hendisantika.read.dto.CategoryReadDTO;
 import com.hendisantika.read.service.CategoryReadService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,5 +31,11 @@ public class CategoryReadController {
 
     public CategoryReadController(CategoryReadService categoryReadService) {
         this.categoryReadService = categoryReadService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryReadDTO>> getAll() {
+        List<CategoryReadDTO> categories = categoryReadService.getAll();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 }
